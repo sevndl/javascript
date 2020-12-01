cee286fcbeec63c0565e75cf034a8485a19bf145
Severin Nandillon <nandillons@gmail.com>
2020-12-01 17:38:12 +0100

    mouvement serpent en cours

--- snake/snake.js
+++ snake/snake.js
@@ -8,7 +8,7 @@ const unite = 30;
 const foodImage = new Image();
 const caseVertClair = new Image();
 const caseVertFonce = new Image();
-foodImage.src = "images/food.jpg";
+foodImage.src = "images/food.png";
 caseVertClair.src = "images/case_vert_clair.png";
 caseVertFonce.src = "images/case_vert_fonce.png";
 
@@ -19,18 +19,47 @@ snake[0] = {
   y: 10 * unite
 };
 
+// direction du serpent
+let d = "RIGHT";
+
+document.addEventListener("keydown", direction);
+function direction(event) {
+  let key = event.key;
+  if (key == "ArrowLeft"  && d != "RIGHT") {
+    d = "LEFT";
+  } else if (key == "ArrowUp" && d != "DOWN") {
+    d = "UP";
+  } else if (key == "ArrowRight" && d != "LEFT")  {
+    d = "RIGHT";
+  } else if (key == "ArrowDown" && d != "UP") {
+    d = "DOWN";
+  }
+} 
 
 // génération aléatoire de la nourriture sur le plateau 
 let food = {
-  x: unite + Math.floor(Math.random()*17 * unite),
-  y: 2 * unite + Math.floor(Math.random()*15 * unite)
+  x: unite + Math.floor(Math.random()*17) * unite,
+  y: 2 * unite + Math.floor(Math.random()*15) * unite
 };
 
 // création du score
 let score = 0;
 
+// vérifie si le serpent se mange la queue
+function seMangeLaQueue(tete, snake) {
+  for (let i = 0; i < snake.length; i++) {
+    if (tete.x == snake[i].x && tete.y == snake[i].y) {
+      return true;
+    }
+  }
+  return false;
+}
+
+//////////////////////////////////////////////////////////////////////
+
 // affichage sur le canvas
 function affichage() {
+
   // affichage du fond
   let alternance = true;
   for (let i = 30; i <= 510; i += unite) {
@@ -46,9 +75,9 @@ function affichage() {
     }
   }
 
-  // affichage de la nourriture
+  // affichage de la nourriture pour le score
   context.drawImage(foodImage, 15, 15, unite, unite);
-  
+
   // affichage de la nourriture aléatoirement sur le plateau
   context.drawImage(foodImage, food.x, food.y, unite, unite);
 
@@ -56,17 +85,44 @@ function affichage() {
   for (let i = 0; i < snake.length; i++) {
     // remplissage du corps
     if (i == 0) {
-      context.fillStyle = "green";
+      context.fillStyle = "red";
     } else {
       context.fillStyle = "white";
     }
     context.fillRect(snake[i].x, snake[i].y, unite, unite);
+  }
 
-    // bordure du serpent
-    context.strokeStyle = "black";
-    context.strokeRect(snake[i].x, snake[i].y, unite, unite);
+  // taille du serpent augmente en fonction de la direction
+  // s'il arrive sur la case avec la nourriture, on laisse l'ancienne tête, sinon on l'enlève
+  let snakeHeadX = snake[0].x;
+  let snakeHeadY = snake[0].y;
+
+  if (d == "LEFT") 
+    snakeHeadX -= unite;
+  if (d == "RIGHT") 
+    snakeHeadX += unite;
+  if (d == "UP")
+    snakeHeadY -= unite;
+  if (d == "DOWN")
+    snakeHeadY += unite;
+
+  // si le serpent mange, on laisse 
+  if (snakeHeadX == food.x && snakeHeadY == food.y) {
+    score += 1;
+    food = {
+      x: unite + Math.floor(Math.random()*17) * unite,
+      y: 2 * unite + Math.floor(Math.random()*15) * unite
+    };
+  } else {
+    snake.pop();
   }
 }
+
+let nouvelleTete = {
+  x: snakeHeadX,
+  y: snakeHeadY
+};
+snake.unshift(nouvelleTete);
 
 // rappeler la fonction tous les 100ms pour mettre à jour l'affichage
 let game = setInterval(affichage, 100);
