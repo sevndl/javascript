c6d3a141be0f5b03b8b4b957837983c9817b23cd
Severin Nandillon <nandillons@gmail.com>
2020-12-07 17:50:12 +0100

    Projet morpion : initial commit -> test récupération click div

---
+++ morpion/test.js
@@ -0,0 +1,7 @@
+document.getElementById("case1").addEventListener("click", function() {
+  document.getElementById("case1").innerHTML = "Click sur case 1";
+});
+
+document.getElementById("case2").addEventListener("click", function() {
+  document.getElementById("case2").innerHTML = "<h1>Click sur case 2</h1>";
+});
